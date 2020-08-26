8f90c996ad20bfb59a5af04ccd569a1aa6dfeed5
lubosson <mksense.happy@gmail.com>
2020-08-26 17:20:35 +0800

    search insert position

---
+++ relax-exp/src/main/java/com/mksense/happy/easy/array/SearchInsertPosition.java
@@ -0,0 +1,38 @@
+package com.mksense.happy.easy.array;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return
+ * the index where it would be if it were inserted in order.
+ *
+ * <p>You may assume no duplicates in the array.
+ *
+ * <p>Example 1:
+ *
+ * <p>Input: [1,3,5,6], 5 Output2 Example 2: *
+ *
+ * <p>Input: [1,3,5,6], 2 Output: 1 Example 3:
+ *
+ * <p>Input: [1,3,5,6], 7 Output: 4 Example 4:
+ *
+ * <p>Input: [1,3,5,6], 0 Output: 0
+ *
+ * @author lubosson
+ * @see com.mksense.happy.easy.array
+ * @since 2020-08-26
+ */
+public class SearchInsertPosition {
+
+	public int searchInsert(int[] nums, int target) {
+		int i = 0;
+
+		while (nums[i] < target) {
+			i++;
+			if (i == nums.length) {
+				return i;
+			}
+		}
+
+		return i;
+	}
+
+}
---
+++ relax-exp/src/test/java/com/mksense/happy/easy/array/SearchInsertPositionTest.java
@@ -0,0 +1,23 @@
+package com.mksense.happy.easy.array;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author lubosson
+ * @see com.mksense.happy.easy.array
+ * @since 2020-08-26
+ */
+public class SearchInsertPositionTest {
+
+  @Test
+  public void searchInsert_right_output() {
+    int[] nums = new int[] { 1, 3, 5, 7, 8, 10 };
+    int target = 11;
+
+    SearchInsertPosition sip = new SearchInsertPosition();
+
+    assertEquals(6, sip.searchInsert(nums, target));
+  }
+}
