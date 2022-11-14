72dc74f02aa4bb6703d2ed52ceb37d01dc1ec407
Luis Lu <mksense.happy@gmail.com>
2022-11-14 15:55:59 +0800

    optimize

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# Leetcode
+# Leetcode daily
+
+
+
 
 Study Plan
-----
-
-
+---
 | plan          | progress    |
 |---------------|-------------|
 | Algorithm I   | in progress |
 | Algorithm II  | planning    |
 | Algorithm III | planning    |
+
+
--- src/main/java/com/mksense/array/easy/MaximumSubarray.java
+++ src/main/java/com/mksense/array/easy/MaximumSubarray.java
@@ -19,6 +19,13 @@ package com.mksense.array.easy;
  */
 public class MaximumSubarray {
 
+	public static void main(String[] args) {
+		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		int expected = 6;
+		MaximumSubarray msa = new MaximumSubarray();
+		System.out.println(msa.maxSubArray(nums));
+	}
+
 	public int maxSubArray(int[] nums) {
 		int max = Integer.MIN_VALUE, sum = 0;
 
